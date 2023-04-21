2e861fc8e0c101d01e3b6935d4dc1d71269a6b5b
wowtrika22 <samymahmoud2532001@gmail.com>
2023-04-21 20:45:25 +0200

    detection the strong of the password

--- js/script.js
+++ js/script.js
@@ -12,3 +12,51 @@ show.onclick = function() {
         show.classList.remove('hide');//replace hide text to show text
     }
 }
+
+//function to return a number representing the strength of the password
+function Strength(password) {
+    let i = 0;
+    if (password.length > 6){
+        i++;
+    }
+    if (password.length >= 10){
+        i++;
+    }
+    if (/[A-Z]/.test(password)){
+        i++;
+    }
+    if (/[0-9]/.test(password)){
+        i++;
+    }
+    if (/[A-Za-z0-8]/.test(password)){
+        i++;
+    }
+    return i;
+}
+
+
+let container = document.querySelector('.container');
+//function to detect if the password is weak or medium or strong
+document.addEventListener('keyup', function(e) {
+    let password = document.querySelector('#myPassword').value;
+    let strength = Strength(password); //call Strengthen function
+    //add class weak strength < 2
+    if (strength<= 2){
+        container.classList.add('weak');
+        container.classList.remove('medium');
+        container.classList.remove('strong');
+    }
+    //add class medium 
+    else if (strength >= 2 && strength<= 4){
+        container.classList.remove('weak');
+        container.classList.add('medium');
+        container.classList.remove('strong');
+    }
+    //add class strong 
+    else 
+    {
+        container.classList.remove('weak');
+        container.classList.remove('medium');
+        container.classList.add('strong');
+    }
+});
